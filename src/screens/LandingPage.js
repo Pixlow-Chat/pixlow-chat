d51c55f083f6b49937b7ec3ec39bacaa8d0ab28f
mypoliticsweb <73365506+mypoliticsweb@users.noreply.github.com>
2021-03-29 11:55:57 -0400

    imported tailwidn, made header component, started landing page, had issues getting font to import

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../components/header";
 
 const LandingPage = () => {
-    return(
-        <div>
-            <h2 style={{color: 'darkslateblue'}}>Text</h2>
-        </div>
-        
-
-    )
-}
+	return (
+		<div>
+			<Header />
+			<div className="bg-black py-32 px-12 object-center mx-auto  ">
+				<div className="flex flex-row-reverse flex-wrap">
+					<div className="md:w-6/12 w-full sm:px-2 md:px-10 px-24">
+						<div className="md:float-left center">
+							<h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl md:text-left text-center text-white">
+								PixlowChat
+							</h1>
+						</div>
+					</div>
+					<div className="md:w-6/12 w-full sm:px-2 md:px-10 px-24">
+						<div className="md:float-right center">
+							<h3 className=" text-center text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+								Make Room
+							</h3>
+							<h3 className="text-center text-white text-lg md:text-xl lg:text-2xl xl:text-3xl">
+								Join Room
+							</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 export default LandingPage;
