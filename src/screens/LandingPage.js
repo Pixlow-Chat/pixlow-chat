364410e3202efda03d8192482d647732d1c7ed54
Amit <abhatna7@stevens.edu>
2021-03-29 17:09:59 -0700

    Implemented basic socket.io implementation (backend completed)

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Chat } from "../components";
 
 const LandingPage = () => {
-    return(
-        <div>
-            <h2 style={{color: 'darkslateblue'}}>Text</h2>
-        </div>
-        
-
-    )
-}
+  return (
+    <div>
+      <h2 style={{ color: "darkslateblue" }}>Text</h2>
+      <Chat />
+    </div>
+  );
+};
 export default LandingPage;
