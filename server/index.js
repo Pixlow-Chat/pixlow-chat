6b00b94642c083349b41cf94699c215533a758ef
Tyler S <tylerseliber@gmail.com>
2021-04-15 08:18:56 -0400

    Changed server port

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
+const port = process.env.PORT || 8000;
 
 io.on("connection", socket => {
     socket.emit("your id", socket.id);
@@ -13,4 +14,4 @@ io.on("connection", socket => {
 })
 
 
-server.listen(8000, () => console.log("server is running on port 8000"));
+server.listen(port, () => console.log("server is running on port ${port}"));
